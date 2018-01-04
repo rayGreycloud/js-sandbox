aec0086e32513d8e7dfbd373a06a58f57e6a5da7
rayGreycloud <rayGreycloud@gmail.com>
2018-01-04 12:33:57 -0800

    Console commands

---
+++ app.js
@@ -0,0 +1,16 @@
+// console commands
+console.time('timer');
+
+console.log('Basic console logging');
+
+console.table({a:1, b:2});
+
+console.error('This is some error');
+
+console.warn('This is a warning');
+
+console.log('Almost done...');
+
+console.timeEnd('timer');
+
+console.clear();
