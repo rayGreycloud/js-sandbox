2b1daf28a469bbf6b3454cf361fc0ead7c08e7d5
rayGreycloud <rayGreycloud@gmail.com>
2018-02-11 08:02:27 -0800

    Add try...catch...finally example

--- tryCatch.js
+++ tryCatch.js
@@ -1,9 +1,23 @@
 // Basic try...catch
+// try {
+//   // intentional error - ReferenceError
+//   myFunction();
+// 
+// } catch (e) {
+//   console.log(e); // prints full error 
+//   console.log(e.name); // ReferenceError
+//   console.log(e.message); // myFunction is not defined 
+// }
+
+// try...catch...finally
 try {
-  // intentional error - call undefined function
   myFunction();
 } catch (e) {
-  console.log(e); // prints full error 
-  console.log(e.name); // ReferenceError
-  console.log(e.message); // 
+  console.log(e.message);
+  // finally runs regardless of result
+} finally {
+  console.log('done!');
 }
+// myFunction is not defined
+// done!
+
