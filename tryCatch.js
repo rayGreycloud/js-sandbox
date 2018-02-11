093f1f62591418aeeb7d76195694a1cf43f42776
rayGreycloud <rayGreycloud@gmail.com>
2018-02-11 08:07:01 -0800

    Add TypeError example

--- tryCatch.js
+++ tryCatch.js
@@ -10,14 +10,25 @@
 // }
 
 // try...catch...finally
-try {
-  myFunction();
-} catch (e) {
-  console.log(e.message);
-  // finally runs regardless of result
-} finally {
-  console.log('done!');
-}
+// try {
+//   myFunction();
+// } catch (e) {
+//   console.log(e.message);
+//   // finally runs regardless of result
+// } finally {
+//   console.log('done!');
+// }
 // myFunction is not defined
 // done!
+
+// TypeError
+try {
+  // intentional error - TypeError
+  null.myFunction();
+} catch (e) {
+  console.log(e.name); // TypeError
+  console.log(e.message); // Cannot read property 'myFunction' of null 
+  console.log(e instanceof TypeError); // true
+  console.log(e instanceof ReferenceError); // false
+}
 
