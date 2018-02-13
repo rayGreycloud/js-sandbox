0326887bd2641750b5ec23395f0df8f1671aaa78
rayGreycloud <rayGreycloud@gmail.com>
2018-02-13 11:43:56 -0800

    Factory pattern example

---
+++ factory.js
@@ -0,0 +1,61 @@
+// Factory pattern
+
+function MemberFactory() {
+  this.createMember = function (name, type) {
+    let member;
+    
+    switch(type) {
+      case 'standard':
+        member = new StandardMembership(name);
+        break;
+      case 'super':
+        member = new SuperMembership(name);        
+        break;
+      default:
+        member = new SimpleMembership(name);        
+    }
+    
+    // if (type === 'simple') {
+    //   member = new SimpleMembership(name);
+    // } else if (type === 'standard') {
+    //   member = new StandardMembership(name);
+    // } else if (type === 'super') {
+    //   member = new SuperMembership(name);
+    // }
+    // 
+    member.type = type;
+    
+    member.define = function () {
+      console.log(`${this.name} (${this.type}): ${this.cost}`);
+    }
+    
+    return member;
+  }
+}
+
+const SimpleMembership = function (name) {
+  this.name = name;
+  this.cost = '$5';
+}
+const StandardMembership = function (name) {
+  this.name = name;
+  this.cost = '$15';
+}
+const SuperMembership = function (name) {
+  this.name = name;
+  this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('Darth', 'super'));
+members.push(factory.createMember('Yoda', 'simple'));
+members.push(factory.createMember('Luke', 'standard'));
+
+console.log(members);
+
+members.forEach(function (member) {
+  member.define();
+});
+
